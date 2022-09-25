02576fd64d3c222ff471764106bc0eb4ec234c57
SohagSagar <sagorsohag29@gmail.com>
2022-09-25 18:12:27 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Login from './Components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
